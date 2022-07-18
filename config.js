8d47b50c7f8822ed3af3139a626e138db4acc920
Cop-Phi <109460361+Cop-Phi@users.noreply.github.com>
2022-07-18 12:30:25 +0700

    Update config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ const CONFIG = {
     btnNo: 'Không, nha anh :((',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại chọn anh để cùng đồng hành dậy 🥺',
     btnReply: 'Gửi cho anh :3',
-    reply: ' ',
+    reply: 'Em cũng không biết sao nữa :> ',
     mess: 'Hehe 🥰. Yêu em nhiều nhiều 😘',
     messDesc: 'Dậy làm bạn gái bé.',
     btnAccept: 'Dạ ❤️',
